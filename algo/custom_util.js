2bce583e778f85336f67c4c0a51ac25573074c01
workofart <hanxiangp@gmail.com>
2018-01-13 22:06:12 -0500

    minor refactor

--- algo/custom_util.js
+++ algo/custom_util.js
@@ -3,40 +3,44 @@ const util = require('util'),
 
 
 const printWalletStatus = () => {
-    let currencyValue = 0;
-    
-    for (let i in global.currencyWallet) {
-        currencyValue += global.currencyWallet[i].qty * global.latestPrice[i]
-    }
+    if (!global.isBacktest) {
+        let currencyValue = 0;
 
-    util.log(`-------------------- Summary ----------------------------`);
-    util.log(`Currency Wallet: ${JSON.stringify(global.currencyWallet, null, 2)}`);
-    util.log(`Starting Value: ${global.INITIAL_INVESTMENT}`);
-    util.log(`---------------------------------`);
-    util.log(`  Market Value: $ ${currencyValue.toFixed(2)}`);
-    util.log(`+ Fiat Wallet:  $ ${global.wallet.toFixed(2)}`);
-    util.log(`= Total Value:  $ ${(currencyValue + global.wallet).toFixed(2)}`);
-    util.log('---------------------------------------------------------\n\n');
+        for (let i in global.currencyWallet) {
+            currencyValue += global.currencyWallet[i].qty * global.latestPrice[i]
+        }
+
+        util.log(`-------------------- Summary ----------------------------`);
+        util.log(`Currency Wallet: ${JSON.stringify(global.currencyWallet, null, 2)}`);
+        util.log(`Starting Value: ${global.INITIAL_INVESTMENT}`);
+        util.log(`---------------------------------`);
+        util.log(`  Market Value: $ ${currencyValue.toFixed(2)}`);
+        util.log(`+ Fiat Wallet:  $ ${global.wallet.toFixed(2)}`);
+        util.log(`= Total Value:  $ ${(currencyValue + global.wallet).toFixed(2)}`);
+        util.log('---------------------------------------------------------\n\n');
+    }
 }
 
 const printPNL = () => {
-    let currencyValue = 0;
+    if (!global.isParamTune) {
+        let currencyValue = 0;
 
-    for (let i in global.currencyWallet) {
-        currencyValue += global.currencyWallet[i].qty * global.latestPrice[i];
+        for (let i in global.currencyWallet) {
+            currencyValue += global.currencyWallet[i].qty * global.latestPrice[i];
+        }
+
+        let numCoins = 0;
+        _.forEach(global.currencyWallet, (coin) => {
+            if (coin.qty > 0) {
+                numCoins++;
+            }
+        });
+
+        // console.log(`Starting Value: $${INITIAL_INVESTMENT}`);
+        let profit = currencyValue + global.wallet - global.INITIAL_INVESTMENT;
+        console.log(`Holding [${numCoins}] coins | $${profit.toFixed(2)} | ${(profit / global.INITIAL_INVESTMENT * 100).toFixed(2)}%`);
+        return (profit / global.INITIAL_INVESTMENT * 100).toFixed(2);
     }
-
-    let numCoins = 0;
-    _.forEach(global.currencyWallet, (coin) => {
-       if (coin.qty > 0) {
-           numCoins++;
-       }
-    });
-
-    // console.log(`Starting Value: $${INITIAL_INVESTMENT}`);
-    let profit = currencyValue + global.wallet - global.INITIAL_INVESTMENT;
-    console.log(`Holding [${numCoins}] coins | $${profit.toFixed(2)} | ${(profit / global.INITIAL_INVESTMENT * 100).toFixed(2)}%`);
-    return (profit / global.INITIAL_INVESTMENT * 100).toFixed(2);
 };
 
 const printBuyHold = () => {
@@ -44,16 +48,16 @@ const printBuyHold = () => {
 };
 
 const printSell = (ticker, price) => {
-    util.log(`************ Sell | ${global.currencyWallet[ticker].qty} [${ticker}] @ ${price} *************`);
+    !global.isParamTune && util.log(`************ Sell | ${global.currencyWallet[ticker].qty} [${ticker}] @ ${price} *************`);
 };
 
 const printBearSell = (ticker, qty, price) => {
-    util.log(`************ Bear Sell | ${qty} [${ticker}] @ ${price} *************`);
+    !global.isParamTune && util.log(`************ Bear Sell | ${qty} [${ticker}] @ ${price} *************`);
 };
 
 
 const printBuy = (ticker, qty, price) => {
-    util.log(`************* Buy | ${qty} [${ticker}] @ ${price} *************`);
+    !global.isParamTune && util.log(`************* Buy | ${qty} [${ticker}] @ ${price} *************`);
 };
 
 const printBacktestSummary = () => {
