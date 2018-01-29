679d0851e6eea850e22cb7bd3514e624eec40873
Henry Pan <hanxiangp@gmail.com>
2018-01-29 02:37:47 +0000

    commited cloud db creds

--- db/config.js
+++ db/config.js
@@ -4,7 +4,7 @@ const client = new Pool({
     user: 'postgres',
     host: 'localhost',
     database: 'portfolio-manager',
-    password: 'henrypan',
+    password: 'Pt2%PB.c,FY}g#UQb6tOfjNM',
     port: 5432
 })
 // Adjust this based on the environment
