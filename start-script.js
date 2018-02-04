31a74c7421f69ebefef7ea4dcb632fe04b9e5727
workofart <hanxiangp@gmail.com>
2018-02-04 14:27:08 -0500

    fix start-script for background binance running

--- start-script.js
+++ start-script.js
@@ -16,17 +16,11 @@ const currentTime = moment().local().format('YYYY-MM-DD_HHmmss').toString();
 
 /*******************************************/
 var logfile = `./logs/bot_${currentTime}.log`;
-console.log(logfile)
-var openFileStream_bot = fs.createWriteStream(logfile, {flags: 'a'});
-openFileStream_bot.on('open', () => {
-    var process = child.spawn('node', ['bot.js'], {cwd: 'algo', shell: true});
-    process.stdout.pipe(openFileStream_bot)
-    process.stderr.pipe(openFileStream_bot)
-})
+console.log(logfile);
+const out = fs.openFileSync(logfile, 'a');
 
-openFileStream_bot.on('error', (err)=> {
-    console.log(err)
-})
+var process = child.spawn('node', ['bot.js'], {cwd: 'algo', detached: true, stdio: ['ignore', out, out]});
+
 /*******************************************/
 
 // Create symbolic link for easy log scanning
